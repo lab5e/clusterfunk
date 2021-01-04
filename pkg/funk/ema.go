64696199f3bffd90002f57fe195dd02fd5c3f5e8
Ståle Dahl <stalehd@gmail.com>
2021-01-04 15:41:11 +0100

    Use EMA with a fixed value for M
    A fixed value of M makes the EMA change more rapidly for new values and
    will respond better to periods of higher latency.

--- pkg/funk/ema.go
+++ pkg/funk/ema.go
@@ -9,21 +9,12 @@ type emaCalculator struct {
 	count int
 }
 
-// Note for future implementations. The EMA can be sluggish to react to
-// abrupt and permanent changes, ie if the latency goes from 10ms to 100ms
-// and stays there so a fixed weight (m) can be used instead to calculate
-// the average. A value of f.e. 0.1 or 0.15 will converge much quicker
-// to the actual average and not remember historical values. This is
-// usually not an issue since the latency is rarely changing for the nodes
-// but if f.e. a slower switch is introduced between nodes or the latency
-// changes for another reason it might not behave as desired.
-
 // newEMACalculator creates a new EMA calculator with the initial value
 // set to the specified value. A value of 0 can be used but it might need a
 // few samples to converge into an usable value.
 func newEMACalculator(initial float64) *emaCalculator {
 	return &emaCalculator{
-		m:     1.0,
+		m:     0.05,
 		ema:   initial,
 		count: 0,
 	}
@@ -32,8 +23,21 @@ func newEMACalculator(initial float64) *emaCalculator {
 // Add adds a new sample to the calculator and calculates a new moving average
 // for the number series. The new moving average is returned.
 func (e *emaCalculator) Add(x float64) float64 {
-	e.count++
-	e.m = 2.0 / (float64(e.count) + 1.0)
+
+	// Note for future implementations. The EMA can be sluggish to react to
+	// abrupt and permanent changes, ie if the latency goes from 10ms to 100ms
+	// and stays there so a fixed weight (m) can be used instead to calculate
+	// the average. A value of f.e. 0.1 or 0.15 will converge much quicker
+	// to the actual average and not remember historical values. This is
+	// usually not an issue since the latency is rarely changing for the nodes
+	// but if f.e. a slower switch is introduced between nodes or the latency
+	// changes for another reason it might not behave as desired.
+	//
+	// Uncomment the following lines to get a classic EMA behaviour:
+	//
+	//e.count++
+	//e.m = 2.0 / (float64(e.count) + 1.0)
+
 	e.ema = (x-e.ema)*e.m + e.ema
 	return e.ema
 }
--- pkg/funk/ema_test.go
+++ pkg/funk/ema_test.go
@@ -6,16 +6,41 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+// classic EMA results below
+/*
+var emaTable [][]float64 = [][]float64{
+	[]float64{100.0, 100.0, 0.0},
+	[]float64{100.0, 100.0, 0.0},
+	[]float64{100.0, 100.0, 0.0},
+	[]float64{50.0, 80.0, 0.0},
+	[]float64{50.0, 70.0, 0.0},
+	[]float64{50.0, 64.286, 0.5},
+	[]float64{50.0, 60.714, 0.5},
+	[]float64{50.0, 58.333, 0.5},
+}*/
+
+var emaTable [][]float64 = [][]float64{
+	[]float64{100.0, 100.0, 0.0},
+	[]float64{100.0, 100.0, 0.0},
+	[]float64{100.0, 100.0, 0.0},
+	[]float64{50, 97.5, 0.0},
+	[]float64{50, 95.125, 0.0},
+	[]float64{50, 92.869, 0.005},
+	[]float64{50, 90.725, 0.005},
+	[]float64{50, 88.689, 0.005},
+	[]float64{50, 86.755, 0.005},
+	[]float64{50, 84.917, 0.005},
+	[]float64{50, 83.171, 0.005},
+	[]float64{50, 81.512, 0.005},
+}
+
 func TestEMA(t *testing.T) {
 	assert := require.New(t)
 
 	ema := newEMACalculator(100)
-	assert.Equal(100.0, ema.Add(100.0))
-	assert.Equal(100.0, ema.Add(100.0))
-	assert.Equal(100.0, ema.Add(100.0))
-	assert.Equal(80.0, ema.Add(50.0))
-	assert.Equal(70.0, ema.Add(50.0))
-	assert.InDelta(64.286, ema.Add(50.0), 0.5)
-	assert.InDelta(60.714, ema.Add(50.0), 0.5)
-	assert.InDelta(58.333, ema.Add(50.0), 0.5)
+
+	for _, v := range emaTable {
+		avg := ema.Add(v[0])
+		assert.InDelta(v[1], avg, v[2])
+	}
 }
